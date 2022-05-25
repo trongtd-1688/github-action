d14f12af0278b868b4b52ba4c574cc988a30e285
trongtran <tran.dinh.trong@sun-asterisk.com>
2022-05-25 08:40:54 +0700

    Test cd 1

--- src/main/java/com/sun/usermanagement/home/HomeController.java
+++ src/main/java/com/sun/usermanagement/home/HomeController.java
@@ -1,7 +1,5 @@
 package com.sun.usermanagement.home;
 
-import com.sun.usermanagement.common.Exception.UserManagementException;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
